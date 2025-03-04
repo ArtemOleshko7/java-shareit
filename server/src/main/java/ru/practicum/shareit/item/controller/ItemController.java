eea71e241c64628f70bc092a20403b11a1348593
Artem Oleshko <freedom2323232323@gmail.com>
2025-03-04 19:12:40 +0300

    ТЗ 16 ревью

--- server/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ server/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -18,37 +18,37 @@ public class ItemController {
     private final ItemService itemService;
 
     @PostMapping
-    ItemDto addItem(@RequestHeader(value = RequestHttpHeaders.USER_ID) int userId,
-                    @RequestBody ItemSaveDto itemSaveDto) {
+    public ItemDto addItem(@RequestHeader(value = RequestHttpHeaders.USER_ID) int userId,
+                           @RequestBody ItemSaveDto itemSaveDto) {
         return itemService.addItem(userId, itemSaveDto);
     }
 
     @PostMapping("/{itemId}/comment")
-    CommentDto addComment(@RequestHeader(value = RequestHttpHeaders.USER_ID) int userId,
-                          @PathVariable int itemId,
-                          @RequestBody CommentSaveDto commentSaveDto) {
+    public CommentDto addComment(@RequestHeader(value = RequestHttpHeaders.USER_ID) int userId,
+                                 @PathVariable int itemId,
+                                 @RequestBody CommentSaveDto commentSaveDto) {
         return itemService.addComment(userId, itemId, commentSaveDto);
     }
 
     @PatchMapping("/{itemId}")
-    ItemDto updateItem(@RequestHeader(value = RequestHttpHeaders.USER_ID) int userId,
-                       @PathVariable int itemId,
-                       @RequestBody ItemSaveDto itemSaveDto) {
+    public ItemDto updateItem(@RequestHeader(value = RequestHttpHeaders.USER_ID) int userId,
+                              @PathVariable int itemId,
+                              @RequestBody ItemSaveDto itemSaveDto) {
         return itemService.updateItem(userId, itemId, itemSaveDto);
     }
 
     @GetMapping("/{itemId}")
-    ItemDto getItem(@PathVariable int itemId) {
+    public ItemDto getItem(@PathVariable int itemId) {
         return itemService.getItem(itemId);
     }
 
     @GetMapping
-    Collection<ItemDto> getAllOwnerItems(@RequestHeader(value = RequestHttpHeaders.USER_ID) int userId) {
+    public Collection<ItemDto> getAllOwnerItems(@RequestHeader(value = RequestHttpHeaders.USER_ID) int userId) {
         return itemService.getAllOwnerItems(userId);
     }
 
     @GetMapping("/search")
-    Collection<ItemDto> searchItems(@RequestParam String text) {
+    public Collection<ItemDto> searchItems(@RequestParam String text) {
         return itemService.searchItems(text);
     }
 }
--- server/src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ server/src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -1,10 +1,7 @@
 package ru.practicum.shareit.user.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.api.ValidateCreateRequest;
-import ru.practicum.shareit.api.ValidateUpdateRequest;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.dto.UserSaveDto;
 import ru.practicum.shareit.user.service.UserService;
@@ -16,23 +13,23 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    UserDto createUser(@RequestBody @Validated(ValidateCreateRequest.class) UserSaveDto userSaveDto) {
+    public UserDto createUser(@RequestBody UserSaveDto userSaveDto) {
         return userService.createUser(userSaveDto);
     }
 
     @GetMapping("/{userId}")
-    UserDto getUser(@PathVariable Integer userId) {
+    public UserDto getUser(@PathVariable Integer userId) {
         return userService.getUser(userId);
     }
 
     @PatchMapping("/{userId}")
-    UserDto updateUser(@PathVariable Integer userId,
-                       @RequestBody @Validated(ValidateUpdateRequest.class) UserSaveDto userSaveDto) {
+    public UserDto updateUser(@PathVariable Integer userId,
+                              @RequestBody UserSaveDto userSaveDto) {
         return userService.updateUser(userId, userSaveDto);
     }
 
     @DeleteMapping("/{userId}")
-    void deleteUser(@PathVariable Integer userId) {
+    public void deleteUser(@PathVariable Integer userId) {
         userService.deleteUser(userId);
     }
 }
